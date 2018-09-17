3d9a56111367bd27d2924badadf5ee77cf210d59
binxu <binxu@binxu-PC>
2018-09-17 10:54:12 +0800

    实体类建立  log4j

---
+++ src/main/java/cn/edu/nyist/xljzspringbootthymeleafmybatisforum/common/model/Comment.java
@@ -0,0 +1,54 @@
+package cn.edu.nyist.xljzspringbootthymeleafmybatisforum.common.model;
+
+import java.io.Serializable;
+
+
+/**
+ * @author binxu
+ *  评论实体类
+ */
+public class Comment implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String content;//评论的内容
+	private int cid;//外键-->帖子card
+	private int uid;//外键 -->用户表user
+
+	public Comment() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getCid() {
+		return cid;
+	}
+
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+
+	public int getUid() {
+		return uid;
+	}
+
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+
+}
