090fa26bd94230b84e2ef45555208ef8e3b9cf10
zhailou <2307283635@qq.com>
2018-09-24 10:25:42 +0800

    邮箱

--- src/main/java/cn/edu/nyist/xljzspringbootthymeleafmybatisforum/controller/UserController.java
+++ src/main/java/cn/edu/nyist/xljzspringbootthymeleafmybatisforum/controller/UserController.java
@@ -4,15 +4,21 @@ import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import cn.edu.nyist.xljzspringbootthymeleafmybatisforum.admin.service.UserService;
@@ -22,7 +28,8 @@ import cn.edu.nyist.xljzspringbootthymeleafmybatisforum.common.model.User;
 public class UserController {
 	@Autowired
 	private UserService userService;
-	
+	@Autowired
+	private JavaMailSender javaMailSender;
 	
 
 	public UserController() {
@@ -38,9 +45,9 @@ public class UserController {
 	//注册
 		@RequestMapping("/reg")
 		public String reg(@ModelAttribute User user,@RequestParam String vcode,@RequestParam MultipartFile photop,HttpServletRequest request,Model model,HttpSession session) throws Exception, IOException {
-			// 先进行验证码验证，验证通过才能访问数据库
-			// 获取服务器端验证码
-			String servserCode = (String) session.getAttribute(ValidVController.serverVcodeName);			
+			// 获取验证码
+			String servserCode= (String) session.getAttribute("Mathcode");
+			System.out.println("----------------------1111111111"+servserCode);
 			//将用户输入的验证码和服务器端进行比较
 			if (!servserCode.equalsIgnoreCase(vcode)) {
 				model.addAttribute("msg", "验证码错误");
@@ -76,17 +83,45 @@ public class UserController {
 	public String toLogin() {
 		return "login";
 	}
+	
+	@RequestMapping("/sendEmail")
+	
+	public String sendMail(@RequestParam String receiver,
+			HttpServletRequest request,HttpSession session) throws MessagingException, IllegalStateException, IOException {
+		//产生四位随机数
+		int num=(int)(Math.random()*(9999-1000+1))+1000;
+		//Double num=Math.random()*3*1000+1;
+		String strnum=String.valueOf(num);
+		System.out.println("++++++++++++++"+strnum);
+		session.setAttribute("Mathcode", strnum);
+		
+		//String servserCode = (String) session.getAttribute(ValidVController.serverVcodeName);	
+		//System.out.println("----------------------111"+servserCode);
+		MimeMessage mailMessage = javaMailSender.createMimeMessage();
+		// 注意添加，否则文件名不正确显示
+		MimeMessageHelper helper = new MimeMessageHelper(mailMessage, true, "utf-8");
+		helper.setFrom("2307283635@qq.com");	
+		helper.setTo(receiver);
+		helper.setText(strnum);
+		System.out.println("----------------------222"+strnum);
+		
+		javaMailSender.send(mailMessage);
+		return "reg";
+
+		
+
+	}
 
 	//登录
 	@RequestMapping(path = "/login")
-	public String login(@RequestParam String name, @RequestParam String pwd, Model model,HttpSession session) {
+	public String login(@RequestParam String name, @RequestParam String pwd,@RequestParam String vcode, Model model,HttpSession session) {
 		// 先进行验证码验证，验证通过才能访问数据库
 		// 获取服务器端验证码
-		/*String servserCode = (String) session.getAttribute(ValidVController.serverVcodeName);
+		String servserCode = (String) session.getAttribute(ValidVController.serverVcodeName);
 		if (!servserCode.equalsIgnoreCase(vcode)) {
 			model.addAttribute("msg", "验证码错误");
 			return "/login";
-		}*/
+		}
 
 		User user = userService.find(name, pwd);
 		System.out.println("========="+user);
@@ -110,6 +145,7 @@ public class UserController {
 		return "redirect:/home";
 	}
 	
+		
 	
 	
 }
